817079bfd6541813c4211bcc83fe38a7d4b27de2
juhallyn <juhallyn@student.42.fr>
2020-10-24 04:38:43 +0200

    create route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <h1> Home </h1>
+          </Route>
+          <Route path="/login">
+            <h1> Login </h1>
+          </Route>
+        </Switch>
+      </Router>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+  </div>
   );
 }
 
